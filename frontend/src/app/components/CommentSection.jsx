9866c568991814702e3d1fc7006ff93699dc1877
danihs2 <daniel.hachac@alumno.buap.mx>
2024-04-09 18:55:06 -0600

    Comment section finalizado, ya implementa api

--- frontend/src/app/components/CommentSection.jsx
+++ frontend/src/app/components/CommentSection.jsx
@@ -1,18 +1,36 @@
 import CommentCard from "./CommentCard";
+import ReplieCard from "./ReplieCard";
 const comments = [
     {
         id: 1,
         id_pelicula: 1,
         id_usuario: 1,
         nombre: "Hugo",
-        comentario: "Genial pelicula!"
+        comentario: "Genial pelicula!",
+        replies: [
+            {
+                id: 1,
+                id_comentario: 1,
+                id_usuario: 2,
+                nombre: "Maria",
+                comentario: "Totalmente de acuerdo! ☺️"
+            },
+            {
+                id: 2,
+                id_comentario: 1,
+                id_usuario: 3,
+                nombre: "Pedro",
+                comentario: "No me gusto mucho"
+            }
+        ]
     },
     {
         id: 2,
         id_pelicula: 1,
         id_usuario: 2,
         nombre: "Maria",
-        comentario: "Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        comentario: "Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        replies: []
     }
 ];
 
@@ -21,11 +39,21 @@ function CommentSection(id_pelicula) {
     <div>
       <h2 className="font-bold text-center mt-5 mb-4">Comentarios</h2>
       {comments.map((comment) => (
+      <div className=" mb-3">
         <CommentCard
           key={comment.id}
           nombre={comment.nombre}
           comentario={comment.comentario}
         />
+        {comment.replies.map((reply) => (
+          <ReplieCard
+            key={reply.id}
+            nombre={reply.nombre}
+            comentario={reply.comentario}
+            reply
+          />
+        ))}
+      </div>
       ))}
     </div>
   );
